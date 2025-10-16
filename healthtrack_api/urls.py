57d78f3a58e5e57e93c410c4dc3f8ed0c3adaf21
Jefferson <jefferson.ig.ig@gmail.com>
2025-10-15 23:16:16 -0500

    run proyect

--- healthtrack_api/urls.py
+++ healthtrack_api/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/v1/pacientes/', permanent=False)), # <-- NEW
     
     path('api/v1/', include('records_app.urls')),
 ]
